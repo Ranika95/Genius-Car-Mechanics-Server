86bb49cfc4544cbe62dd47281f5ab730222699bf
Ranika95 <ranikapavin@gmail.com>
2022-09-10 15:52:21 +0200

    added port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
